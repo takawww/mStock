5f707d2a2d5993894796801f8b036f28884f106b
Taka Wong <taka_www@yahoo.com.hk>
2016-02-19 17:12:14 +0800

    Add localstore

--- js/stockFunc.js
+++ js/stockFunc.js
@@ -8,6 +8,7 @@ function getStock(){
 		crossDomain: true, 
 		success: function (result) {
 			//var obj = (JSON.parse(result));
+			if (localStorage.getItem("stockPrices") == null) localStorage.setItem("stockPrices", JSON.stringify(result.stockPrices));
 			setIndexPrices(result.stockIndexs);
 			setStockPrices(result.stockPrices);
 			showIndexGrid(stockIndexs);
@@ -49,14 +50,13 @@ function setIndexPrices(dataSet){
 					stockIndexs[j].percent = dataSet[i].percent;
 					stockIndexs[j].createDate = dataSet[i].createDate;
 					stockIndexs[j].createTime = dataSet[i].createTime;
-
-
 				}
 			}
 			if(!isFound){
 				stockIndexs.push(dataSet[i].code);
 			}
 		}
+		localStorage.setItem("stockPrices", JSON.stringify(stockPrices))
 	}
 }
 
@@ -74,6 +74,21 @@ function setStockPrices(dataSet){
 				if (stockPrices[j].code == dataSet[i].code)
 				{
 					isFound = true;
+					if (stockPrices[j].alarm != null)
+					{
+						if (stockPrices[j].alarm == "Y" && stockPrices[j].alarmVal != "")
+						{
+							var alarmVal = parseFloat( stockPrices[j].alarmVal);
+							var changePercent = (parseFloat(dataSet[i].price) - parseFloat(stockPrices[j].price))/parseFloat(dataSet[i].price);
+							if ( changePercent > alarmVal)
+							{
+								javascript:playAudio('soundUp');
+							}else if (changePercent < (-1 * alarmVal))
+							{
+								javascript:playAudio('soundDown');
+							}
+						}
+					}
 					stockPrices[j].name = dataSet[i].name;
 					stockPrices[j].price = dataSet[i].price;
 					stockPrices[j].delta = dataSet[i].delta;
@@ -272,6 +287,7 @@ function showStockInfo(stockID){
 		if (stockPrices[i].code == stockID)
 		{
 			stock = stockPrices[i];
+			break;
 		}
 	}
 	var sHTML = "";
@@ -335,23 +351,51 @@ function showStockInfo(stockID){
 	sHTML = sHTML + "		<div class='divStockObjLow'>" + stock.low + "</div>"
 	sHTML = sHTML + "	</div>"	
 	sHTML = sHTML + "</div>"
-
-	//sHTML = sHTML + "<div class='divDetailsCode'>" + stock.code + "</div>"
-	//sHTML = sHTML + "<div class='divDetailsName'>" + stock.name + "</div>"
-	//sHTML = sHTML + "<div class='divDetailsPrice'>" + stock.price + "</div>"
-	//sHTML = sHTML + "<div class='divDetailsDelta'>" + stock.delta + "</div>"
-	//sHTML = sHTML + "<div class='divDetailsHighLow'></div>"
-	//sHTML = sHTML + "<div class='divDetailsNotice'>"
-	//sHTML = sHTML + "</div>"
-	
+	sHTML = sHTML + "<div class='divDetailsChart' style='background-image:url(http://charts.aastocks.com/servlet/Charts?com=50004&stockid=" + stock.code + ".HK&period=0&footerStyle=1&lang=1&scheme=0&height=160&width=160)'>&nbsp;</div>";
+	sHTML = sHTML + "<div class='divDetailsAlertSetRow'><input type='checkbox' id='chkAlarmSet' onclick='javascript:showAlarmSetting(this.checked)'>自動提示 (+/- %)</div>"
+	sHTML = sHTML + "<div class='divDetailsAlertSet divDetailsAlertSetRow'><input type='number' id='txtAlarmValue' class='txtStockAlarm'></div>"
+	sHTML = sHTML + "<div class='divDetailsAlertSet divDetailsAlertSetRow'><input type='button' value='設定' class='btnStockAlarm' onclick='setStockAlarm(\"" + stock.code + "\")'></div>"
 	
 	$(".divOverlayDetails").hide('slide', {direction: 'right'}, 1000, function(){
 		$(".divOverlayContent").html(sHTML);
+		if (stock.alarm != null)
+		{
+			if (stock.alarm == "Y")
+			{
+
+				$(".divDetailsAlertSet").show();
+				$("#chkAlarmSet").prop("checked", true);
+				$("#txtAlarmValue").val(stock.alarmVal);
+			}
+		}
 	});
 	
 	$(".divOverlayDetails").show('slide', {direction: 'right'}, 1000);
 	
 }
+
+function showAlarmSetting(checked){
+	if(checked){
+		$(".divDetailsAlertSet").show();
+	}else{
+		$(".divDetailsAlertSet").hide();
+	}
+}
+
+function setStockAlarm(stockID){
+	var stock;
+	for(var i=0;i<stockPrices.length;i++){
+		if (stockPrices[i].code == stockID)
+		{
+			stockPrices[i].alarm = "Y";
+			stockPrices[i].alarmVal = $("#txtAlarmValue").val();
+			break;
+		}
+	}
+	localStorage.setItem("stockPrices", JSON.stringify(stockPrices));
+	hideStockInfo();
+}
+
 
 function hideStockInfo(){
 	$(".divOverlayDetails").hide('slide', {direction: 'right'}, 1000);
